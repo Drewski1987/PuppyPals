45de7f91e3bdd9085dbb51894b54263c50f726af
Maurice Drew <drewm1987@gmail.com>
2025-03-26 14:17:08 -0500

    finishing touch

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,41 @@ import { useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
 
+
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   console.log("puppyList: ", puppyList);
+  console.log(featuredPup)
+
   return (
     <>
+
+{featPupId && (
+      <div>
+      <h1>{featuredPup.name}</h1>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+      </div>
+    )}
+    
       <div className='App'>
         {
           puppies.map((puppy)=> {
-            return <p key= {puppy.id}>{puppy.name}</p>;
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key= {puppy.id}>{puppy.name}</p>;
           })
           
         }
+       
       </div>
-      
-    </>
-  )
+      </>
+    
+  );
 }
+        
 
 export default App
